4b3c323eefe0c12b4aebadcadfe5f2ee3141d4cc
naushairch <naushairumair905@gmail.com>
2026-03-01 02:19:32 +0500

    complete code

--- phase-2/plugins/inputs.py
+++ phase-2/plugins/inputs.py
@@ -11,11 +11,19 @@ class JsonReader:
     def __init__(self, file_path: str, service: PipelineService):
         self.file_path = file_path
         self.service = service
+    def run(self) -> None:
+        """
+        Entry point to start reading data.
+        """
+
+        with open(self.file_path, "r") as file:
+            data = json.load(file)
+
+        # Send raw data to core
+        self.service.execute(data)
 
 
 
 
 
 
-
-        
