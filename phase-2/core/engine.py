ea3fcce7845b5b80aa9243dd70674642e8ada6cf
naushairch <naushairumair905@gmail.com>
2026-03-01 23:27:50 +0500

    morechanges

--- phase-2/core/engine.py
+++ phase-2/core/engine.py
@@ -54,3 +54,90 @@ class TransformationEngine:
         sorted_data = sorted(filtered, key=lambda r: r[year], reverse=True)
 
         return sorted_data[:10]
+    # 2 BOTTOM 10
+    # -------------------------
+    def bottom_10(self, data):
+        continent = self.config["continent"]
+        year = str(self.config["year"])
+
+        filtered = list(
+            filter(lambda r: r["Continent"] == continent and r.get(year), data)
+        )
+
+        sorted_data = sorted(filtered, key=lambda r: r[year])
+
+        return sorted_data[:10]
+
+    # -------------------------
+    # 3 GROWTH RATE
+    # -------------------------
+    def growth_rate(self, data):
+        continent = self.config["continent"]
+        start = str(self.config["start_year"])
+        end = str(self.config["end_year"])
+
+        result = []
+
+        for row in data:
+            if row["Continent"] == continent:
+                if row.get(start) and row.get(end):
+                    growth = ((row[end] - row[start]) / row[start]) * 100
+                    result.append({
+                        "Country": row["Country Name"],
+                        "Growth Rate (%)": round(growth, 2)
+                    })
+
+        return result
+
+    # -------------------------
+    # 4 AVERAGE BY CONTINENT
+    # -------------------------
+    def average_by_continent(self, data):
+        start = str(self.config["start_year"])
+        end = str(self.config["end_year"])
+
+        continent_totals = {}
+
+        for row in data:
+            continent = row["Continent"]
+
+            if row.get(start) and row.get(end):
+                avg = (row[start] + row[end]) / 2
+
+                if continent not in continent_totals:
+                    continent_totals[continent] = []
+
+                continent_totals[continent].append(avg)
+
+        result = []
+
+        for cont, values in continent_totals.items():
+            result.append({
+                "Continent": cont,
+                "Average GDP": round(sum(values) / len(values), 2)
+            })
+
+        return result
+
+    # -------------------------
+    # 5 GLOBAL TREND
+    # -------------------------
+    def total_global_trend(self, data):
+        start = self.config["start_year"]
+        end = self.config["end_year"]
+
+        result = []
+
+        for year in range(start, end + 1):
+            year_str = str(year)
+            total = sum(
+                row.get(year_str, 0) or 0
+                for row in data
+            )
+
+            result.append({
+                "Year": year,
+                "Total Global GDP": total
+            })
+
+        return result
