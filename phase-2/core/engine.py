e5e685f1de0268045f25e6d1641326584666cad4
naushairch <naushairumair905@gmail.com>
2026-03-01 22:22:30 +0500

    computingfunctionadded

--- phase-2/core/engine.py
+++ phase-2/core/engine.py
@@ -19,3 +19,24 @@ class TransformationEngine(PipelineService):
         filtered_by_continent = list(
             filter(lambda record: record.get("Continent") == continent, raw_data)
         )
+        # Step 2: Map to compute GDP sum between years
+        def compute_total(record):
+            years = [
+                value for key, value in record.items()
+                if key.isdigit() and start_year <= key <= end_year
+            ]
+
+            # convert to float safely
+            numbers = list(
+                map(lambda x: float(x) if x not in (None, "", "null") else 0.0, years)
+            )
+
+            total = reduce(lambda a, b: a + b, numbers, 0.0)
+
+            return {
+                "Country": record.get("Country Name", "Unknown"),
+                "Total GDP": total
+            }
+
+        mapped_data = list(map(compute_total, filtered_by_continent))
+        
